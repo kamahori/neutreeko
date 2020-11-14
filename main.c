f2114274a54383aaf88798e0bedafec2b2a127a8
masahiro-negishi <72982764+masahiro-negishi@users.noreply.github.com>
2020-11-14 14:37:48 +0900

    Update main.c
    compute_output関数とminimax関数を作りました。あと、盤面の状態とスコアを保持する構造体を導入しました。minimax法では5手先までを全探索しています。

--- main.c
+++ main.c
@@ -1,26 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int b[5][5]; //4Eはb[3][4],2Bはb[1][1],というように…y座標、x座標の順
-int answer[9][2];
+; //4Eはb[3][4],2Bはb[1][1],というように…y座標、x座標の順
+int answer[8][2];
+
+//boardは盤面の状態stateと盤面の点数pointを保持する構造体
+typedef struct board {
+    int state[5][5];
+    int point;
+} board;
+
+board b;
 
 int BLACK = 1;
 int WHITE = 2;
 int DEBUG = 0;
 
-void init(void) { //初期化関数
+void init() { //初期化関数
     int x, y;
-    for (x = 0; x < 5; x++){ //基本全部空
-        for (y = 0; y < 5; y++){
-            b[x][y] = 0; 
+    for (x = 0; x < 5; x++) { //基本全部空
+        for (y = 0; y < 5; y++) {
+            b.state[x][y] = 0;
         }
     }
-    b[0][1] = BLACK; //B1には黒
-    b[0][3] = BLACK; //D1には黒
-    b[3][2] = BLACK; //C4には黒
-    b[4][1] = WHITE; //B5には白
-    b[4][3] = WHITE; //D5には白
-    b[1][2] = WHITE; //C2には白
+    b.state[0][1] = BLACK; //B1には黒
+    b.state[0][3] = BLACK; //D1には黒
+    b.state[3][2] = BLACK; //C4には黒
+    b.state[4][1] = WHITE; //B5には白
+    b.state[4][3] = WHITE; //D5には白
+    b.state[1][2] = WHITE; //C2には白
 }
 
 void print(void) {
@@ -28,52 +36,65 @@ void print(void) {
 
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 5; j++) {
-            printf("%d", b[i][j]);
+            printf("%d", b.state[i][j]);
         }
         printf("\n");
     }
 }
 
-int is_finished(void) {
+int is_finished(board b) {
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 5; j++) {
             for (int k = 0; k < 3; k++) {
-                if (b[i][j] != 0 && b[i][j] == b[i + 1][j] && b[i + 1][j] == b[i + 2][j]) return 1;
-                if (b[j][i] != 0 && b[j][i] == b[j][i + 1] && b[j][i + 1] == b[j][i + 2]) return 1;
-                if (b[i][k] != 0 && b[i][k] == b[i + 1][k + 1] && b[i + 1][k + 1] == b[i + 2][k + 2]) return 1;
-                if (b[i + 2][k] != 0 && b[i + 2][k] == b[i + 1][k + 1] && b[i + 1][k + 1] == b[i][k + 2]) return 1;
+                if (b.state[i][j] != 0 && b.state[i][j] == b.state[i + 1][j] && b.state[i + 1][j] == b.state[i + 2][j]) return 1;
+                if (b.state[j][i] != 0 && b.state[j][i] == b.state[j][i + 1] && b.state[j][i + 1] == b.state[j][i + 2]) return 1;
+                if (b.state[i][k] != 0 && b.state[i][k] == b.state[i + 1][k + 1] && b.state[i + 1][k + 1] == b.state[i + 2][k + 2]) return 1;
+                if (b.state[i + 2][k] != 0 && b.state[i + 2][k] == b.state[i + 1][k + 1] && b.state[i + 1][k + 1] == b.state[i][k + 2]) return 1;
             }
         }
     }
     return 0;
 }
 
-void moveable(int y, int x) { 
+void moveable(int y, int x) {
     //座標を受け取って動ける場所を列挙する関数
     //answerに動ける場所全てが格納される 
-    int i, j, s, k;
-    for (i = -1; i <= 1; i++){
-        for (j = -1; j <= 1; j++){ //全方位探索
-            for (s = 1; s < 5; s++){ //その方位でどこまで行けるか
-    
-                if (y + j * s < 0 || y + j * s >= 5 || x + i * s < 0 || x + i * s >= 5){
+    int i;
+    for (i = 0; i < 8; i++) { //answerの初期化
+        answer[i][0] = 100;
+        answer[i][1] = 100;
+    }
+
+    int j, s, k;
+    k = 0;
+    for (i = -1; i <= 1; i++) {
+        for (j = -1; j <= 1; j++) { //全方位探索
+
+            if (i == 0 && j == 0) {
+                continue;
+            }
+
+            for (s = 1; s < 5; s++) { //その方位でどこまで行けるか
+
+                if (y + j * s < 0 || y + j * s >= 5 || x + i * s < 0 || x + i * s >= 5) {
                     break;
                 }
-            
-                if (b[y + j * s][x + i * s] == 0){ //進んだ先のマスが空なら
+
+                if (b.state[y + j * s][x + i * s] == 0) { //進んだ先のマスが空なら
                     answer[k][0] = y + j * s; //answerのi+j-2（方角のパラメータ）にそのマスを書き込む
                     answer[k][1] = x + i * s;
-                } else {
+                }
+                else {
                     break;
                 }
             }
             k++;
         }
     }
- 
+
 }
 
-int is_valid_move(char *input) {
+int is_valid_move(char* input) {
     if (input[0] < '0' || input[0] > '4') return 0;
     if (input[1] < 'A' || input[1] > 'E') return 0;
     if (input[2] < '0' || input[2] > '4') return 0;
@@ -85,29 +106,24 @@ void get_input(int color) { //石を動かす関数
     char input[5];
     int y, x, w, z, i;
 
-    scanf("%s", input); //入力"4E2C"などを受け取って
+    scanf_s("%s", input); //入力"4E2C"などを受け取って
 
-    y = input[0] - 49; //ASCIIコードを座標に変換
-    x = input[1] - 65;
-    w = input[2] - 49;
-    z = input[3] - 65;
+    y = input[0] - '1'; //ASCIIコードを座標に変換
+    x = input[1] - 'A';
+    w = input[2] - '1';
+    z = input[3] - 'A';
 
-    if (!is_valid_move(input) || b[y][x] != color) {
+    if (!is_valid_move(input) || b.state[y][x] != color) {
         puts("Error");
         exit(1);
     }
 
-    for (i = 0; i < 9; i++){ //answerの初期化
-        answer[i][0] = 100;
-        answer[i][1] = 100;
-    }
-
     moveable(y, x);
 
-    for (i = 0; i < 9; i++){
-        if (answer[i][0] == w && answer[i][1] == z){ //石が移動できるなら
-            b[y][x] = 0; //元いた場所を空にして
-            b[w][z] = color; //移動する
+    for (i = 0; i < 9; i++) {
+        if (answer[i][0] == w && answer[i][1] == z) { //石が移動できるなら
+            b.state[y][x] = 0; //元いた場所を空にして
+            b.state[w][z] = color; //移動する
             return;
         }
     }
@@ -117,12 +133,85 @@ void get_input(int color) { //石を動かす関数
     exit(1);
 }
 
+typedef struct move {
+    //(x1, y1)から(x2, y2)への遷移
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+}move;
+
+typedef struct next_board{
+    move travel[24];
+}next_board;
+
+board minimax(board x, int depth, int color) {
+    if (depth == 0) {
+        x.point = 0;
+        return x;
+    }
+    int i, j, k;
+    int count = 0;
+    next_board next;
+
+    //盤面から可能な遷移をnextにいれる。
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < 5; j++) {
+            if (x.state[i][j] == color) {
+
+                moveable(i, j);
+
+                for (k = 0; k < 8; k++) {
+                    if (!answer[k][0] != 100) {
+                        next.travel[count].x1 = i;
+                        next.travel[count].y1 = j;
+                        next.travel[count].x2 = answer[k][0];
+                        next.travel[count].y2 = answer[k][1];
+                        count++;
+                    }
+                }
+            }
+        }
+    }
+    board ans;
+    for (i = 0; i < count; i++) {
+        board y = x;
+        //遷移前のマスを0に、遷移先のマスをcolorに
+        y.state[next.travel[i].x1][next.travel[i].y1] = 0;
+        y.state[next.travel[i].x2][next.travel[i].y2] = color;
+
+        if (is_finished(y)) {
+            y.point = 100;
+            return y;
+        }
+        board z = minimax(y, depth - 1, color);
+        if (i == 0) {
+            ans = z;
+            continue;
+        }
+        if (depth % 2 == 1) {
+            if (z.point > ans.point) {
+                ans = z;
+            }
+        }
+        else {
+            if (z.point < ans.point) {
+                ans = z;
+            }
+        }
+    }
+    ans.point -= 1;
+    return ans;
+}
+
 void compute_output(int color) {
     // コンピュータの色がcolor
     // bの値を変化させ、動きをprint
+    b = minimax(b, 5, color);
+    print();
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     init();
     int cnt = 0;
 
@@ -133,7 +222,7 @@ int main(int argc, char *argv[]) {
             get_input(BLACK);
             cnt++;
             print();
-            if (is_finished()) {
+            if (is_finished(b)) {
                 puts("You Win");
                 exit(0);
             }
@@ -142,7 +231,7 @@ int main(int argc, char *argv[]) {
             compute_output(WHITE);
             cnt++;
             print();
-            if (is_finished()) {
+            if (is_finished(b)) {
                 puts("You Lose");
                 exit(0);
             }
@@ -152,14 +241,15 @@ int main(int argc, char *argv[]) {
                 exit(0);
             }
         }
-    } else {
+    }
+    else {
         // コンピュータが先手(黒)
         while (1) {
             // CPUの手番
             compute_output(BLACK);
             cnt++;
             print();
-            if (is_finished()) {
+            if (is_finished(b)) {
                 puts("You Lose");
                 exit(0);
             }
@@ -168,7 +258,7 @@ int main(int argc, char *argv[]) {
             get_input(WHITE);
             cnt++;
             print();
-            if (is_finished()) {
+            if (is_finished(b)) {
                 puts("You Win");
                 exit(0);
             }
