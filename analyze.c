c3ea2c4a6f45b7dd15668c098735fdcc85533873
kamahori <keisuke1258@gmail.com>
2020-11-17 15:35:43 +0900

    Refactor analyze.c

--- analyze.c
+++ analyze.c
@@ -3,106 +3,87 @@
 #include <string.h>
 #include <limits.h>
 
+#define SIZE 999983 // size for hashtable
+
 const int BLACK = 1;
 const int WHITE = 2;
-const int MAX_DEPTH = 1e5;
+const int MAX_SCORE = 1e5;
 const int INF = 1e8;
 const int COMB_LEN = 2500;
 
-/*
-   A B C D E
-  ----------
-5| u v w x y 
-4| p q r s t 
-3| k l m n o 
-2| f g h i j 
-1| a b c d e 
-
-    A  B  C  D  E
-  ---------------
-5| 20 21 22 23 24
-4| 15 16 17 18 19
-3| 10 11 12 13 14
-2|  5  6  7  8  9
-1|  0  1  2  3  4
-*/
-
-//keyは状態を表すint、dataはそのscore
-
-#define SIZE 999983
+// location of pawn is expressed by an integer between 0 and 24
+// state of board is expressed by 5 * 6 = 30 bit integer
+//     A  B  C  D  E
+//   ---------------
+// 5| 20 21 22 23 24
+// 4| 15 16 17 18 19
+// 3| 10 11 12 13 14
+// 2|  5  6  7  8  9
+// 1|  0  1  2  3  4
 
 typedef struct DI {
+    // data element of hashtable
     int data;
     int key;
 } DataItem;
 
-//DataItemのポインタの配列、DataItemのポインタ
-DataItem *hashArray[SIZE];
-
-//ハッシュ関数の定義
-int hash_code(int key){
-    return key % SIZE;
-}
-
-//線形探索法による検索
-DataItem *hash_search(int key)
-{
-    int hashIndex = hash_code(key);
-    while(hashArray[hashIndex] != NULL){
-        if(hashArray[hashIndex]->key == key) {
-            return hashArray[hashIndex];
-        }
-        hashIndex++;
-        hashIndex %= SIZE;
-    }
-    //見つからなかった場合
-    return NULL;
-}
-
-//挿入
-void hash_insert(int key, int data){
-    DataItem *item = (DataItem *)malloc(sizeof(DataItem));
-    item->data = data;
-    item->key = key;
-    int hashIndex = hash_code(key);
-    while(hashArray[hashIndex] != NULL && hashArray[hashIndex]->key != -1){
-        hashIndex++;
-        hashIndex %= SIZE;
-    }
-    hashArray[hashIndex] = item;
-}
-
-// listによるqueueの実装
-
 typedef struct node{
+    // node of queue
     struct node* next;
     int value;
 } Node;
 
 typedef struct tag{
+    // data format of queue
     Node* front;
     Node* tail;
 } Tag;
 
+// hashtable
+DataItem *hash_array[SIZE];
+
+int hash_code(int key) {
+    // hash function
+    return key % SIZE;
+}
+
+DataItem *hash_search(int key) {
+    // search key from hashtable linearly
+    int hashIndex = hash_code(key);
+    while(hash_array[hashIndex] != NULL){
+        if(hash_array[hashIndex]->key == key) {
+            return hash_array[hashIndex];
+        }
+        hashIndex++;
+        hashIndex %= SIZE;
+    }
+    // when not found
+    return NULL;
+}
+
+void hash_insert(int key, int data) {
+    // insert data to hashtable
+    DataItem *item = (DataItem *)malloc(sizeof(DataItem));
+    item->data = data;
+    item->key = key;
+    int hashIndex = hash_code(key);
+    while(hash_array[hashIndex] != NULL && hash_array[hashIndex]->key != -1){
+        hashIndex++;
+        hashIndex %= SIZE;
+    }
+    hash_array[hashIndex] = item;
+}
+
 Tag* queue_create(){
+    // implement queue by list
     Tag* queue = (Tag*)malloc(sizeof(Tag));
     (*queue).front = NULL;
     (*queue).tail = NULL;
     return queue;
 }
 
-void queue_delete(Tag* queue){
-    Node* p = (*queue).front;
-    Node* tmp;
-    while(p != NULL) {
-        tmp = (*p).next;
-        if (p != NULL) free(p);
-        p = tmp;
-    }
-    if (queue != NULL) free(queue);
-}
-
 void queue_enqueue(Tag* queue, int value){
+    // push a value to the tail
     Node* p = (Node*)malloc(sizeof(Node));
     (*p).value = value;
     (*p).next = NULL;
@@ -116,8 +97,9 @@ void queue_enqueue(Tag* queue, int value){
 }
 
 int queue_dequeue(Tag* queue){
-    //queueが空の時は、-1を返す。それ以外の時は先頭のvalueを返す
+    // pop the front value of queue
     if((*queue).front == NULL){
+        // when queue is empty
         return -1;
     }
     Node* p = (*queue).front;
@@ -131,6 +113,7 @@ int queue_dequeue(Tag* queue){
 }
 
 int sort_state(const int state) {
+    // make `state` increasing order for each player
     int res = 0;
 
     int b[3] = {(state >> 25) & 0b11111, (state >> 20) & 0b11111, (state >> 15) & 0b11111};
@@ -174,6 +157,7 @@ int sort_state(const int state) {
 }
 
 int mirror(const int state) {
+    // flip the current board horizontally given state in int
     int res = 0;
     for (int i = 0; i < 6; i++) {
         int tmp = (state >> (25 - i * 5)) & 0b11111;
@@ -184,6 +168,7 @@ int mirror(const int state) {
 }
 
 int rotate(const int state) {
+    // rotate the current board given state in int
     int res = 0;
     for (int i = 0; i < 6; i++) {
         int tmp = (state >> (25 - i * 5)) & 0b11111;
@@ -195,6 +180,8 @@ int rotate(const int state) {
 }
 
 int equiv(const int state) {
+    // only consider one situation out of 8 equivalent situations
+    // that can be reached by mirroring or rotation
     int res = sort_state(state);
     int tmp = state;
 
@@ -222,26 +209,31 @@ int equiv(const int state) {
 }
 
 int switch_player(const int x) {
+    // change first and second players given state in int
     int y = (x & ((1 << 15) - 1)) << 15;
     y |= (x >> 15);
     return equiv(y);
 }
 
 int is_won(int x, int color) {
+    // judge if player of `color` has won the game
     if (color == WHITE) return is_won(switch_player(x), BLACK);
 
     int a = (x >> 25) & 0b11111;
     int b = (x >> 20) & 0b11111;
     int c = (x >> 15) & 0b11111;
 
+    // horizontally
     if (a / 5 == b / 5 && b / 5 == c / 5) {
         if (a + 1 == b && b + 1 == c) {
             return 1;
         }
     }
 
+    // vertically
     if (a + 5 == b && b + 5 == c) return 1;
 
+    // diagonally
     if (a / 5 + 1 == b / 5 && b / 5 + 1 == c / 5) {
         if (a + 6 == b && b + 6 == c) return 1;
         if (a + 4 == b && b + 4 == c) return 1;
@@ -251,6 +243,7 @@ int is_won(int x, int color) {
 }
 
 void recursive_comb(int** res, int* positions, int* indexes, int s, int rest, int* iter) {
+    // compute combination and store them to `res`
     if (rest == 0) {
         for (int i = 0; i < 3; i++) res[*iter][i] = indexes[i];
         *iter += 1;
@@ -264,11 +257,13 @@ void recursive_comb(int** res, int* positions, int* indexes, int s, int rest, in
 }
 
 int** comb(int* positions) {
-    // recursive_comb とあわせて，25C3または22C3を全列挙する
-    // positions[i] == -1 であるようなiは数えない
+    // along with recursive_comb, enumerate all three combinations out of 25 or 22 locations
+    // `positions` is used to prevent multiple pawns from locating at the same position
+    // when positions[i] == -1, location i is used by the opposite player
     int indexes[3];
     
     int **res = (int**)malloc(sizeof(int*) * COMB_LEN);
+    // COMB_LEN > 25C3
     for (int i = 0; i < COMB_LEN; i++) {
         res[i] = (int*)malloc(sizeof(int) * 3);
     }
@@ -283,18 +278,14 @@ int** comb(int* positions) {
 }
 
 Tag* get_all_states() {
-    // ありうるすべての状態を列挙
+    // list every possible situation
+    // each situation is expressed by one int
     Tag* res = queue_create();
-    // std::vector<int> res;
-    // int* res = (int*)malloc(sizeof(int) * LIST_LEN); // 4e6 > 25C3 * 22C3
-    // for (int i = 0; i < LIST_LEN; i++) {
-    //     res[i] = 0;
-    // }
     int positions[25];
-    // int iter = 0;
 
     for (int i = 0; i < 25; i++) positions[i] = i;
     int** comb1 = comb(positions);
+    // comb1 contains the locations of first player's pawns
 
     for (int i = 0; i < COMB_LEN; i++) {
         if (comb1[i][2] == 0) continue;
@@ -302,15 +293,19 @@ Tag* get_all_states() {
         for (int i = 0; i < 25; i++) remain[i] = i;
         for (int k = 0; k < 3; k++) remain[comb1[i][k]] = -1;
         int** comb2 = comb(remain);
+        // comb1 contains the locations of second player's pawns
         for (int j = 0; j < COMB_LEN; j++) {
             if (comb2[j][2] == 0) continue;
-            int state = (comb1[i][0] << 25) | (comb1[i][1] << 20) | (comb1[i][2] << 15) | (comb2[j][0] << 10) | (comb2[j][1] << 5) | comb2[j][2];
+            int state = (comb1[i][0] << 25) 
+                        | (comb1[i][1] << 20) 
+                        | (comb1[i][2] << 15) 
+                        | (comb2[j][0] << 10) 
+                        | (comb2[j][1] <<  5) 
+                        | (comb2[j][2] <<  0);
             if (state != equiv(state)) continue;
-            if (is_won(state, BLACK) && is_won(state, WHITE)) continue; // 両者が同時に勝つような場合は除外
-            // res[iter] = state;
-            // iter++;
-            // printf("%d\n", state);
-            // res.push_back(state);
+            // only consider one out of 8 equivalent situations
+            if (is_won(state, BLACK) && is_won(state, WHITE)) continue;
+            // two players cannot win at the same time
             queue_enqueue(res, state);
         }
         if (comb2 != NULL) free(comb2);
@@ -322,18 +317,18 @@ Tag* get_all_states() {
 
 int* get_moves(const int x) {
     // given a certain state, return list of 
-    // all valid states after one step by black
+    // all valid states after one step by first player
     int* res = (int*)malloc(sizeof(int) * 24);
-    for (int i = 0; i < 24; i++) {
-        res[i] = 0;
-    }
+    for (int i = 0; i < 24; i++) res[i] = 0;
+
+    // convert state to 5 * 5 matrix
     int board[5][5] = {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}};
     board[((x >> 25) & 31) / 5][((x >> 25) & 31) % 5] = BLACK;
     board[((x >> 20) & 31) / 5][((x >> 20) & 31) % 5] = BLACK;
     board[((x >> 15) & 31) / 5][((x >> 15) & 31) % 5] = BLACK;
     board[((x >> 10) & 31) / 5][((x >> 10) & 31) % 5] = WHITE;
-    board[((x >> 5) & 31) / 5][((x >> 5) & 31) % 5] = WHITE;
-    board[(x & 31) / 5][(x & 31) % 5] = WHITE;
+    board[((x >>  5) & 31) / 5][((x >>  5) & 31) % 5] = WHITE;
+    board[((x >>  0) & 31) / 5][((x >>  0) & 31) % 5] = WHITE;
     int iter = 0;
 
     for (int pawn = 0; pawn < 3; pawn++) {
@@ -341,13 +336,14 @@ int* get_moves(const int x) {
         int b = ((x >> (25 - 5 * pawn)) & 31) % 5;
         for (int i = -1; i <= 1; i++) {
             for (int j = -1; j <= 1; j++) {
-                // res[iter] = -1;
+                // i and j are for direction
                 if (i == 0 && j == 0) continue;
                 for (int s = 1; s < 5; s++) {
-                    if (a + i * s < 0 || a + i * s >= 5 || b + j * s < 0 || b + j * s >= 5) continue;
+                    // s is for distance
+                    if (a + i * s < 0 || a + i * s >= 5 || b + j * s < 0 || b + j * s >= 5) break;
 
                     if (board[a + i * s][b + j * s] == 0) {
-                        // (a, b) -> (a + j * s, b + i * s)
+                        // move from (a, b) to (a + j * s, b + i * s)
                         int tmp = x;
                         int mask = ((1 << 30) - 1) - ((1 << (30 - 5 * pawn)) - 1) + ((1 << (25 - 5 * pawn)) - 1);
                         tmp &= mask;
@@ -366,58 +362,31 @@ int* get_moves(const int x) {
 }
 
 int main() {
-
-    // std::map<int, int> scores;
-    // std::map<int, int> next_move;
-    // int* scores = (int*)malloc(sizeof(int) * (1 << 30));
-    // int* next_move = (int*)malloc(sizeof(int) * (1 << 30));
-    // for (int i = 0; i < (1 << 30); i++) {
-    //     scores[i] = 0;
-    //     next_move[i] = 0;
-    // }
-    // printf("done\n");
-
     Tag* states = get_all_states();
     Tag* neutrals = queue_create();
-    // int* states = get_all_states();
-    // int* neutrals = (int*)malloc(sizeof(int) * LIST_LEN);
-    // for (int i = 0; i < LIST_LEN; i++) {
-    //     neutrals[i] = 0;
-    // }
-    // neutrals にはその時点で最善手を決められてないものを入れる
-    int iter = 0;
+    // `neutrals` stores neutral (= yet to be determined next movement) situations at that time
+    int cnt = 0;
     
-    // どちらかが勝っているものをスコア付け
+    // give sufficiently big or small score to situations that one player is winning
     while(1) {
         int tmp_state = queue_dequeue(states);
         if (tmp_state == -1) break;
-        // if (states[i] == 0) {
-        //     continue; // valid state cannot be 0
-        // }
 
         if (is_won(tmp_state, BLACK)) {
-            hash_insert(tmp_state, MAX_DEPTH);
-            // scores[states[i]] = MAX_DEPTH;
+            hash_insert(tmp_state, MAX_SCORE);
         } else if (is_won(tmp_state, WHITE)) {
-            hash_insert(tmp_state, -MAX_DEPTH);
-            // scores[states[i]] = -MAX_DEPTH;
+            hash_insert(tmp_state, -MAX_SCORE);
         } else {
             queue_enqueue(neutrals, tmp_state);
-            // neutrals.push_back(tmp_state);
-            iter++;
+            cnt++;
         }
     }
     
     int prev = -1;
-    while (prev != iter) {
-        // これ以上進まなくなるまで，すでにスコア付けされている状態の一手前にスコア付け
-        prev = iter;
-        // printf("while\n");
-        iter = 0;
-        // int* tmp = (int*)malloc(sizeof(int) * LIST_LEN);
-        // for (int i = 0; i < LIST_LEN; i++) {
-        //     tmp[i] = 0;
-        // }
+    while (prev != cnt) {
+        // give score to situations that is one step before situations which is already scored
+        prev = cnt;
+        cnt = 0;
         Tag* tmp = queue_create();
         
         while (1) {
@@ -429,57 +398,30 @@ int main() {
             int* moves = get_moves(tmp_state);
             for (int j = 0; j < 24; j++) {
                 if (moves[j] == 0) continue;
-                // if (scores[moves[j]] < min) {
-                //     min = scores[moves[j]];
-                //     best_move = moves[j];
-                // }
+                // choose next movement so that the score for the opposite player minimizes
                 int opposite = switch_player(moves[j]);
                 DataItem *opposite_data = hash_search(opposite);
                 int opposite_score;
                 if (opposite_data == NULL) opposite_score = 0;
                 else opposite_score = opposite_data->data;
-                // if (opposite_data->data == NULL) opposite_score = 0;
-                // else opposite_score = opposite_data->data;
                 if (opposite_score < min) {
                     min = opposite_score;
                     best_move = moves[j];
                 }
             }
             if (min == 0) {
-                // tmp[iter] = neutrals[i];
-                // tmp.push_back(neutrals[i]);
                 queue_enqueue(tmp, tmp_state);
-                iter++;
+                cnt++;
                 continue;
             }
 
             int score = min < 0 ? -1 - min : 1 - min;
-            // scores[neutrals[i]] = score;
             hash_insert(tmp_state, score);
-            // if (min < 0) scores[neutrals[i]] = -1 - min;
-            // else scores[neutrals[i]] = 1 - min;
 
-            // next_move[neutrals[i]] = best_move;
-
+            // save current state, next movement, and current score to use during the match
             printf("%d,%d,%d\n", tmp_state, best_move, score);
         }
 
         neutrals = tmp;
-        // queue_delete(tmp);
-
-        // neutrals.resize(tmp.size());
-        // for (int i = 0; i < tmp.size(); i++) neutrals[i] = tmp[i];
-        
-        // memcpy(neutrals, tmp, sizeof(int) * LIST_LEN);
-        // free(tmp);
     }
-
-    // for (int i = 0; i < LIST_LEN; i++) {
-    //     if (scores[i] != 0) {
-    //         printf("%d,%d,%d\n", i, scores[i], next_move[i]);
-    //     }
-    // }
-
-    // free(states);
-    // free(neutrals);
 }
